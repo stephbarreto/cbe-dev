22841bab5c79fa3168577b2bd43dfbfec9682aca
Stephanny <stephbarreto@me.com>
2020-11-19 12:14:14 -0300

    subindo arquivos do gitlab

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+var countDownDate = new Date("Oct 29, 2020 00:00:00").getTime();
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("full").innerHTML = hours + ":"
+  + minutes + ":" + seconds + "";
+    
+//    document.getElementById("short").innerHTML = hours + ":"
+//  + minutes;
+    
+}, 1000);
+
+console.log(x);
+
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
